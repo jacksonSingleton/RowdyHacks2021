c050e503a3e7afde3be828fd4d31b5b413d9c000
Jackson Singleton <jackson@tiberon.co>
2021-03-27 12:29:03 -0500

    updated emmiter code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as PIXI from "pixi.js";
-
+import * as particles from "pixi-particles";
 
 const app = new PIXI.Application({
     width: 1280,
@@ -7,40 +7,80 @@ const app = new PIXI.Application({
     transparent: true
 });
 document.body.appendChild(app.view);
-
-let circleCount = 15;
-
-let fireCircles = [];
-
-const fireColors = [
-    0x261A18,
-    0xA65233,
-    0xF27F3D,
-    0xF2AC57,
-    0xF2E750
-];
-
-const fire = new PIXI.Graphics();
-
-app.stage.addChild(fire);
-app.stage.interactive = true;
-window.app = app;
-
-function getColor(colors){
-    return colors[Math.floor(Math.random() * colors.length)]
-}
-function circleSize(min, max){
-    return Math.floor(Math.random() * (max - min) + min);
-}
-function createFire(event) {
-    fire.beginFill(getColor(fireColors));
-    fire.drawCircle(event.data.global.x, event.data.global.y, circleSize(5,20))
-    fire.endFill();
-    console.log("mouse pressed");
-}
-let tick = 0;
-app.ticker.add(() => {
-     
-    app.renderer.plugins.interaction.on('mousedown', createFire);
-    tick += 0.1;
+let container = new PIXI.Container();
+var emitter = new particles.Emitter(
+   container,
+{
+	"alpha": {
+		"start": 0.68,
+		"end": 0.11
+	},
+	"scale": {
+		"start": 0.15,
+		"end": 0.6,
+		"minimumScaleMultiplier": 3
+	},
+	"color": {
+		"start": "#fade6c",
+		"end": "#eb5905"
+	},
+	"speed": {
+		"start": 250,
+		"end": 300,
+		"minimumSpeedMultiplier": 1
+	},
+	"acceleration": {
+		"x": 3,
+		"y": -2500
+	},
+	"maxSpeed": 250,
+	"startRotation": {
+		"min": 7,
+		"max": 360
+	},
+	"noRotation": false,
+	"rotationSpeed": {
+		"min": 0,
+		"max": 0
+	},
+	"lifetime": {
+		"min": 0.28,
+		"max": 0.9
+	},
+	"blendMode": "normal",
+	"frequency": 0.001,
+	"emitterLifetime": -1,
+	"maxParticles": 500,
+	"pos": {
+		"x": 0,
+		"y": 0
+	},
+	"addAtBack": false,
+	"spawnType": "point"
 })
+
+// Calculate the current time
+var elapsed = Date.now();
+
+// Update function every frame
+var update = function(){
+
+    // Update the next frame
+    requestAnimationFrame(update);
+
+    var now = Date.now();
+
+    // The emitter requires the elapsed
+    // number of seconds since the last update
+    emitter.update((now - elapsed) * 0.001);
+    elapsed = now;
+
+    // Should re-render the PIXI Stage
+    // renderer.render(stage);
+};
+
+// Start emitting
+emitter.emit = true;
+
+// Start the update
+update();
