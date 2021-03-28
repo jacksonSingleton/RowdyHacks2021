c92f35f081b257f2ea7c82f6a801d7b189dba633
lkarvec <lkarvec@gmail.com>
2021-03-28 08:58:45 -0500

    panic

--- src/pages/Incindio.js
+++ src/pages/Incindio.js
@@ -1,8 +1,84 @@
 import React from 'react';
 import styled from "styled-components";
+import * as PIXI from "pixi.js";
+require("../spells/Particle.js");
+
+
+const app = new PIXI.Application({
+    backgroundAlpha: false
+});
+app.loader.add('room', '/src/assets/lumosBG.png');
+app.loader.load(setup);
+function setup(loader, resources) {
+    const background = new PIXI.Sprite(resources.room.texture);
+    app.stage.addChild(background);
+    background.width = app.screen.width;
+    background.height = app.screen.height;
+
+    app.stage.interactive = true;
+
+}
+const Incindio = () => {
+    return (
+        document.body.appendChild(app.view)
+    );
+};
+document.body.appendChild(app.view);
+new ParticleExample(
+	["./assets/particle.png"],
+{
+	"alpha": {
+		"start": 0.68,
+		"end": 0.11
+	},
+	"scale": {
+		"start": 0.1,
+		"end": 0.6,
+		"minimumScaleMultiplier": 3
+	},
+	"color": {
+		"start": "#fade6c",
+		"end": "#d94d1f"
+	},
+	"speed": {
+		"start": 250,
+		"end": 300,
+		"minimumSpeedMultiplier": 1
+	},
+	"acceleration": {
+		"x": 3,
+		"y": -2500
+	},
+	"maxSpeed": 250,
+	"startRotation": {
+		"min": 7,
+		"max": 360
+	},
+	"noRotation": false,
+	"rotationSpeed": {
+		"min": 0,
+		"max": 0
+	},
+	"lifetime": {
+		"min": 0.28,
+		"max": 0.9
+	},
+	"blendMode": "add",
+	"frequency": 0.001,
+	"emitterLifetime": -1,
+	"maxParticles": 500,
+	"pos": {
+		"x": 0,
+		"y": 0
+	},
+	"addAtBack": false,
+	"spawnType": "point"
+})
+
+
 function play() {
     
-    var audio = new Audio('../assets/PatronusSound.wav');
+    var audio = new Audio('../assets/nice.wav');
     audio.play();
   }
 // Create a Title component that'll render an <h1> tag with some styles
@@ -19,13 +95,7 @@ const Wrapper = styled.section`
   background: #1b1b1b;
 `;
 
-const Incindio = () => {
-    return (
-        <Wrapper>
-            <Title>Incindio</Title>
-        </Wrapper>
-    );
-};
+
 
 export default Incindio;
 
