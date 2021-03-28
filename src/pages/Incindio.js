ead892ca2171beb0f53211cfb59946a8e485331a
lkarvec <lkarvec@gmail.com>
2021-03-28 08:35:14 -0500

    Update Incindio.js

--- src/pages/Incindio.js
+++ src/pages/Incindio.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from "styled-components";
-
+function play() {
+    
+    var audio = new Audio('../assets/PatronusSound.wav');
+    audio.play();
+  }
 // Create a Title component that'll render an <h1> tag with some styles
 const Title = styled.h1`
   font-size: 1.5em;
@@ -24,3 +28,68 @@ const Incindio = () => {
 };
 
 export default Incindio;
+
+function listenToUser(phrase){
+    var transcript = "";
+    var confidence = "";
+    // New speech recognition object
+    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+    var recognition = new SpeechRecognition();
+    //recognition.continuous = true;
+
+    // This runs when the speech recognition service starts
+    recognition.onstart = function() {
+        console.log("Voice Recognition listening. Speak into the microphone.");
+    };
+
+    recognition.onspeechend = function() {
+        // Stop when user is done speaking
+        recognition.stop();
+    }
+
+    // This runs when the speech recognition service returns result
+    recognition.onresult = function(event) {
+        transcript = event.results[0][0].transcript;
+        confidence = event.results[0][0].confidence;
+        console.log(transcript);
+        
+        if (('incendio' === transcript) == true) {
+            onPlayVideo();
+            console.log("video played");
+        }
+    };
+
+    // start recognition
+    recognition.start();
+     
+    ret = false;
+}
+
+const button = new PIXI.Graphics()
+    .beginFill(0x0, 0.5)
+    .drawRoundedRect(0, 0, 100, 100, 10)
+    .endFill()
+    .beginFill(0xffffff)
+    .moveTo(36, 30)
+    .lineTo(36, 70)
+    .lineTo(70, 50);
+
+// Position the button
+button.x = (app.screen.width - button.width) / 2;
+button.y = (app.screen.height - button.height) / 2;
+
+// Enable interactivity on the button
+button.interactive = true;
+button.buttonMode = true;
+
+// Add to the stage
+app.stage.addChild(button);
+
+// Listen for a click/tap event to start playing the video
+// this is useful for some mobile platforms. For example:
+// ios9 and under cannot render videos in PIXI without a
+// polyfill - https://github.com/bfred-it/iphone-inline-video
+// ios10 and above require a click/tap event to render videos
+// that contain audio in PIXI. Videos with no audio track do
+// not have this requirement
+button.on('pointertap', listenToUser);
