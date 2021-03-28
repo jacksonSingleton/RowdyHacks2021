3abdfd83177bb15b24d560877c614ffd0dbd8399
lkarvec <lkarvec@gmail.com>
2021-03-28 05:18:38 -0500

    update patronus.js

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
 import Card from '../components/Card'
+
 // Create a Title component that'll render an <h1> tag with some styles
 const Title = styled.h1`
   font-size: 1.5em;
